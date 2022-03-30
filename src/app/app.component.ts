1fd2731fa445999bb71cb9b7c66a2ce684bd4956
Martin Pastorino <martinpastorino013@gmail.com>
2022-03-30 20:23:28 -0300

    se instalo bootstrap y se creo diferentes metodos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadoraAngular';
+  title = 'Mini Calculadora';
+  numeroUno: number = 0;
+  numeroDos: number = 0;
+  resultado: number = 0;
+
+  sumar():void{
+    this.resultado= this.numeroUno+ this.numeroDos;
+  }
+
+  resta():void {
+    this.resultado= this.numeroUno -this.numeroDos;
+  }
+
+  multiplicar():void {
+    this.resultado= this.numeroUno *this.numeroDos;
+  }
+  dividir():void {
+    this.resultado= this.numeroUno /this.numeroDos;
+  }
 }
